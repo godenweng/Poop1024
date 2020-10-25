eb135afeb44a81d5928c8defa121c6302f7220bf
godenweng <godenweng@gmail.com>
2020-10-25 14:08:28 +0800

    example till pp.32

---
+++ demo19.py
@@ -0,0 +1,11 @@
+from common.const import MAX_CONCURRENT_JOBS,PI,GRAVITY
+print(PI)
+print(MAX_CONCURRENT_JOBS)
+print(GRAVITY)
+
+PI *=2
+MAX_CONCURRENT_JOBS +=100
+GRAVITY = GRAVITY - 1
+print(PI)
+print(MAX_CONCURRENT_JOBS)
+print(GRAVITY)
