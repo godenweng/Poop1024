fb7a5101e49a04595c3a7982653430379c524b46
godenweng <godenweng@gmail.com>
2020-10-25 12:00:20 +0800

    floating and comlex

---
+++ demo15.py
@@ -0,0 +1,23 @@
+x1 = 'a'
+x2 = "a"
+x3 = '''a'''
+x4 = """a"""
+x5 = "Hello World"
+x6 = b"Hello World"
+x7 = u"Hello World"
+x8 = 'hello\nworld'
+x9 = 'hello\tworld'
+x10 = 'ˋ中文'
+print(type(x1),type(x2),type(x3),type(x4))
+print(type(x5))
+print(type(x6))
+print(type(x7))
+print('x1 = ',x1)
+print('x2 = ',x2)
+print('x3 = ',x3)
+print('x4 = ',x4)
+print('x5 = ',x5)
+print('x6 = ',x6)
+print('x8 = ',x8)
+print('x9 = ',type(x9),x9)
+print('x10 = ',type(x10),x10)
