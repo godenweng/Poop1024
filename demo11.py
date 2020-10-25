39fedafe2672f5939efbc968628c2be6924d4dbd
godenweng <godenweng@gmail.com>
2020-10-25 10:18:21 +0800

    int dec <==> bin, oct, hex

---
+++ demo11.py
@@ -0,0 +1,21 @@
+x = 5
+print(type(x), x)
+y = 2 ** 31 - 1
+print(y)
+z = 2 ** 64
+print(z)
+t = 2 ** 512
+print(t)
+a1 = 135
+print(a1)
+print(">{}".format(a1))
+print(">>{:b}".format(a1))
+print(">>>{:o}".format(a1))
+print(">>>>{:x}".format(a1))
+b1 = 0b10101010
+print(b1)
+b2 = 0o20204477
+print(b2)
+b3 = 0xFFEEDDCC
+b4 = 0xC0FFEE
+print(b3, b4)
