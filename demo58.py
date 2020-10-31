9110c0db24f3b3e5be65ef919a80aa9a8f21b785
godenweng <godenweng@gmail.com>
2020-10-31 15:16:10 +0800

    pass

---
+++ demo58.py
@@ -0,0 +1,23 @@
+def funcl():
+    pass
+
+
+for _ in range(10):
+    pass
+
+if 1 > 0:
+    pass
+
+while False:
+    pass
+
+
+class T1:
+    pass
+
+
+class T1:
+    def xyz(self):
+        pass
+
+    pass
