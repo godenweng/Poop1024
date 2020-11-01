19126d8eb4568fd823f566be9c96cde7429baa7f
godenweng <godenweng@gmail.com>
2020-11-01 09:20:32 +0800

    lambda

---
+++ demo69.py
@@ -0,0 +1,12 @@
+import math
+
+def square_root(x):
+    return math.sqrt(x)
+
+def square_root2(x): return math.sqrt(x)
+
+square_root3 = lambda x: math.sqrt(x)
+
+print(square_root(5))
+print(square_root2(6))
+print(square_root3(7))
